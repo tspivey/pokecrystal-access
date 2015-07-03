e1288a6fc5b29e2978ab424ce000a9ea1c9f976b
bradjrenshaw <bradjrenshaw@gmail.com>
2015-07-03 11:50:12 -0400

    Removed broken code from navigation.lua.

--- navigation.lua
+++ navigation.lua
@@ -1,21 +1,9 @@
 announce_navigation = true
-announce_objects = false
-
+directions = {"up", "down", "left", "right"}
 previous_tiles = {-1, -1, -1, -1}
 tiles = {-1, -1, -1, -1}
-objects = {{}, {}, {}, {}}
-directions = {"up", "down", "left", "right"}
-coords_directions = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}
 
 
-function has_key(t, key)
-for k, v in pairs(t) do
-if k==key then
-return true
-end
-end
-return false
-end
 
 function read_tiles()
 local down = memory.readbyte(0xc2fa)
@@ -26,38 +14,16 @@ previous_tiles = {tiles[1], tiles[2], tiles[3], tiles[4]}
 tiles = {up, down, left, right}
 end
 
-function near_objects()
-local player_x, player_y = get_player_xy()
-o = get_map_info()
-o = o.objects
-objects = {{}, {}, {}, {}}
-for i=1, #o do
-if o[i].type~="connection" then
-relative_x = o[i].x-player_x
-relative_y = o[i].y-player_y
-print(o[i].name..": "..relative_x..", "..relative_y)
-for d=1, #coords_directions do
-if relative_x == coords_directions[d][1] and relative_y==coords_directions[d][2] then
-objects[d] = o[i]
-end
-end
-end
-end
-end
-
 function announce_tiles()
 read_tiles()
 near_objects()
 announce = ""
-if announce_navigation==false and announce_objects==false then
+if announce_navigation==false then
 return
 end
 for d = 1,#directions do
-if (announce_objects and has_key(objects[d], "type")) or (announce_navigation and tiles[d]~=previous_tiles[d]) then
+if announce_navigation and tiles[d]~=previous_tiles[d] then
 announce = announce..directions[d].." "
-if announce_objects and has_key(objects[d], "type") then
-announce = announce..objects[d].name.." "
-end
 if announce_navigation and tiles[d]~=previous_tiles[d] then
 announce = announce..tiles[d].." "
 end
