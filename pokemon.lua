d8a15cecfab633c4c5732936d6b849be07fd454f
Daniel Dugovic <dandydand@gmail.com>
2020-04-03 11:45:09 -0500

    Test that trail has a direction, distance, and a path

--- pokemon.lua
+++ pokemon.lua
@@ -373,35 +373,7 @@ end
 function read_tiles()
 local player_x, player_y = get_player_xy()
 local collisions = get_map_collisions()
-local s = string.format("Now %d", collisions[player_y][player_x])
-
--- Check up tile
-if player_y > 1 then
-	s = s .. string.format(", Up %d", collisions[player_y - 1][player_x])
-else -- up is none
-	s = s .. ", Up none"
-end -- Check up tile
-
--- Check down tile
-if player_y < #collisions then
-	s = s .. string.format(", Down %d", collisions[player_y + 1][player_x])
-else -- Down is none
-	s = s .. ", Down none"
-end -- Check down tile
-
--- Check left tile
-if player_x > 1 then
-	s = s .. string.format(", Left %d", collisions[player_y][player_x - 1])
-else -- left is none
-	s = s .. ", Left none"
-end -- Check left tile
-
--- Check right tile
-if player_x < #collisions[0] then
-	s = s .. string.format(", Right %d", collisions[player_y][player_x + 1])
-else -- right is none
-	s = s .. ", Right none"
-end -- Check right tile
+local s = route(player_y, player_x, collisions)
 
 tolk.output(s)
 end
@@ -626,7 +598,7 @@ local x, y = get_player_xy()
 local map_id = get_map_id()
 local s = get_name(mapid, item)
 if item.x then
-s = s .. ": " .. direction(x, y, item.x, item.y)
+s = s .. ": " .. distance(x, y, item.x, item.y)
 end
 if item.facing then
 s = s .. " facing " .. facing_to_string(item.facing)
@@ -800,18 +772,6 @@ path = astar.path(start, dest, allnodes, true, valid)
 return path
 end
 
-function clean_path(path)
-local start = path[1]
-local new_path = {}
-for i, node in ipairs(path) do
-if i > 1 then
-local last = path[i-1]
-table.insert(new_path, only_direction(last.x, last.y, node.x, node.y))
-end -- i > 1
-end -- for
-return group_unique_items(new_path)
-end
-
 function speak_path(path)
 for _, v in ipairs(path) do
 tolk.output(v[2] .. " " .. v[1])
@@ -924,25 +884,6 @@ tolk.output("no bar found")
 else
 tolk.output(enemy_health)
 end
-end
-
-function group_unique_items(t)
-if #t == 0 then return t end
-if #t == 1 then return {{t[1], 1}} end
-local nt = {}
-local last = t[1]
-local last_count = 1
-for i = 2, #t do
-if t[i] == last then
-last_count = last_count + 1
-else
-table.insert(nt, {last, last_count})
-last = t[i]
-last_count = 1
-end
-end
-table.insert(nt, {last, last_count})
-return nt
 end
 
 function read_keyboard()
--- trail.lua
+++ trail.lua
@@ -1,6 +1,18 @@
 module ( "trail", package.seeall )
 
-function direction(x, y, destx, desty)
+function clean_path(path)
+local start = path[1]
+local new_path = {}
+for i, node in ipairs(path) do
+if i > 1 then
+local last = path[i-1]
+table.insert(new_path, direction(last.x, last.y, node.x, node.y))
+end -- i > 1
+end -- for
+return group_unique_items(new_path)
+end
+
+function distance(x, y, destx, desty)
 local s = ""
 if y > desty then
 s = y-desty .. " up"
@@ -15,7 +27,7 @@ end
 return s
 end
 
-function only_direction(x, y, destx, desty)
+function direction(x, y, destx, desty)
 local s = ""
 if y > desty then
 return "up"
@@ -26,6 +38,59 @@ return "left"
 elseif x < destx then
 return "right"
 end
+return s
+end
+
+function group_unique_items(t)
+if #t == 0 then return t end
+if #t == 1 then return {{t[1], 1}} end
+local nt = {}
+local last = t[1]
+local last_count = 1
+for i = 2, #t do
+if t[i] == last then
+last_count = last_count + 1
+else
+table.insert(nt, {last, last_count})
+last = t[i]
+last_count = 1
+end
+end
+table.insert(nt, {last, last_count})
+return nt
+end
+
+function route(player_y, player_x, collisions)
+local s = string.format("Now %d", collisions[player_y][player_x])
+
+-- Check up tile
+if player_y > 1 then
+	s = s .. string.format(", Up %d", collisions[player_y - 1][player_x])
+else -- up is none
+	s = s .. ", Up none"
+end -- Check up tile
+
+-- Check down tile
+if player_y < #collisions then
+	s = s .. string.format(", Down %d", collisions[player_y + 1][player_x])
+else -- Down is none
+	s = s .. ", Down none"
+end -- Check down tile
+
+-- Check left tile
+if player_x > 1 then
+	s = s .. string.format(", Left %d", collisions[player_y][player_x - 1])
+else -- left is none
+	s = s .. ", Left none"
+end -- Check left tile
+
+-- Check right tile
+if player_x < #collisions then
+	s = s .. string.format(", Right %d", collisions[player_y][player_x + 1])
+else -- right is none
+	s = s .. ", Right none"
+end -- Check right tile
+
 return s
 end
 
